1246e5c77634d4c314a5a8f45de0685c78670809
helloyucom <wsy89@sina.cn>
2019-08-10 23:32:53 +0800

    tow

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" abc="ACD"/>
+        <!--<HelloWorld :msg="msg" />-->
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'home',
+        // data() {
+        //     return {msg: "abc"}
+        // },
+        components: {
+            HelloWorld
+        }
+    }
+</script>
