6912a757ef49cf3ef2978e1829b18d79e86cb69f
helloyucom <wsy89@sina.cn>
2019-08-12 09:59:04 +0800

    add createComponents

--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="main">
         <Header title="我的喵喵"></Header>
+        <div id="content">
+            <Login></Login>
+        </div>
         <TabBar></TabBar>
     </div>
 </template>
@@ -8,11 +11,13 @@
 <script>
     import Header from '@/components/Header'
     import TabBar from '@/components/TabBar'
+    import Login from '@/components/Login'
     export default {
         name: "Mine",
         components: {
             Header,
-            TabBar
+            TabBar,
+            Login
         }
     }
 </script>
